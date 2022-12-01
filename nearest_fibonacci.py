ce21dfb90afa3ac144d6c900444ec0544149651d
dilkhush04 <99658871+dilkhush04@users.noreply.github.com>
2022-12-01 22:39:52 +0530

    n=int(input()) a=1 b=0 c=0 while(1>0):     if(c<=n):         min=c     if(c>=n):         max=c         break     c=a+b     a=b     b=c if(n-min==max-n):     print(min,max) elif(n-min<max-n):     print(min) else:     print(max)

--- nearest_fibonacci.py
+++ nearest_fibonacci.py
@@ -1,29 +1,20 @@
 n=int(input())
 a=1
 b=0
-p=4
 c=0
-temp=c
-for i in range(n**2):
-     if(c<=n):
-          s=c
-     if(c>=n):
-          p=c
-          break
-     c=a+b
-     a=b
-     b=c
-if(s==n):
-    print(s)
-elif((n-s)<(p-n)):
-    print(s)
-elif((n-s)==(p-n)):
-    print(s,p)
+while(1>0):
+    if(c<=n):
+        min=c
+    if(c>=n):
+        max=c
+        break
+    c=a+b
+    a=b
+    b=c
+if(n-min==max-n):
+    print(min,max)
+elif(n-min<max-n):
+    print(min)
 else:
-    print(p)
-
-   
-                    
-                
-                    
-        
+    print(max)
+    
