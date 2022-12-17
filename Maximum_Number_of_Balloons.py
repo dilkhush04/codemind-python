29f90e2b2f3c43a1371d69da62a1c77447182ad5
dilkhush04 <99658871+dilkhush04@users.noreply.github.com>
2022-12-18 00:55:06 +0530

    def balloon(text):     bCount = 0     aCount = 0     lCount = 0     oCount = 0     nCount = 0     for ch in text:         if ch == "b":             bCount += 1         elif ch == "a":             aCount += 1         elif ch == "l":             lCount += 1         elif ch == "o":             oCount += 1         elif ch == "n":             nCount += 1     return min(bCount, aCount, lCount//2, oCount//2, nCount) s=input() print(balloon(s))

---
+++ Maximum_Number_of_Balloons.py
@@ -0,0 +1,20 @@
+def balloon(text):
+    bCount = 0
+    aCount = 0
+    lCount = 0
+    oCount = 0
+    nCount = 0
+    for ch in text:
+        if ch == "b":
+            bCount += 1
+        elif ch == "a":
+            aCount += 1
+        elif ch == "l":
+            lCount += 1
+        elif ch == "o":
+            oCount += 1
+        elif ch == "n":
+            nCount += 1
+    return min(bCount, aCount, lCount//2, oCount//2, nCount)
+s=input()
+print(balloon(s))            
