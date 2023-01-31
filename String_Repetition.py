2a159f7c97e058a7f90aad2d4eb44f08e8c77c98
MD DILKHUSH <99658871+dilkhush04@users.noreply.github.com>
2023-02-01 01:16:38 +0530

    s=str(input()) n=int(input()) s=list(s) c=s.count('a') if(n%len(s)==0):     res=n//len(s)     print(res*c) else:     x=0     res=n//len(s)     res1=n%len(s)     for i in range(res1):         if s[i]=='a':             x=x+1     print(res*c+x)

---
+++ String_Repetition.py
@@ -0,0 +1,15 @@
+s=str(input())
+n=int(input())
+s=list(s)
+c=s.count('a')
+if(n%len(s)==0):
+    res=n//len(s)
+    print(res*c)
+else:
+    x=0
+    res=n//len(s)
+    res1=n%len(s)
+    for i in range(res1):
+        if s[i]=='a':
+            x=x+1
+    print(res*c+x)
