df9d759936300943af31faa38bb4e4369465bb0f
MD DILKHUSH <99658871+dilkhush04@users.noreply.github.com>
2023-02-01 17:47:58 +0530

    s=input() s=s.split(" ") temp=-999 res=0 for word in s:     c=0     word=list(word)     for i in word:         if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):             c=c+1     if(c>=temp):         temp=c for word in s:     x=0     word=list(word)     for i in word:         if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):             x=x+1     if(x==temp):         res=res+1 print(res)

---
+++ count_words_with_maximum_vowels.py
@@ -0,0 +1,24 @@
+s=input()
+s=s.split(" ")
+temp=-999
+res=0
+for word in s:
+    c=0
+    word=list(word)
+    for i in word:
+        if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):
+            c=c+1
+    if(c>=temp):
+        temp=c
+for word in s:
+    x=0
+    word=list(word)
+    for i in word:
+        if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):
+            x=x+1
+    if(x==temp):
+        res=res+1
+print(res)
+    
+    
+            
